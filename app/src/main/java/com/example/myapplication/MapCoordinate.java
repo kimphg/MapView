fd22aeb0f28715b45e811663a7e0f629e9bf1bb2
MinhHieu <rezo.krifferson@gmail.com>
2019-09-27 09:46:32 +0700

    MapCoordinate class to calculate the distance

---
+++ app/src/main/java/com/example/myapplication/MapCoordinate.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import android.graphics.PointF;
+
+public class MapCoordinate {
+    public PointF getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(PointF coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    private PointF coordinate;
+
+    protected double distanceToOtherCoord(@org.jetbrains.annotations.NotNull MapCoordinate otherCoord ){
+        final double R = 6371;
+
+        double dlat = R * Math.abs( coordinate.y - otherCoord.coordinate.y );
+        double dlong = R * Math.abs( coordinate.x - otherCoord.coordinate.x );
+
+        double distance = Math.sqrt( Math.pow( dlat, 2 ) + Math.pow( dlong, 2 ));
+
+        return distance;
+    }
+
+    //convert from kilometers to nautical miles
+    protected double convertKmToNm( double value ){
+        return value / 1.852d;
+    }
+
+    //convert from nautical miles to kilometers
+    protected double convertNmToKm( double value ){
+        return value * 1.852;
+    }
+
+}
