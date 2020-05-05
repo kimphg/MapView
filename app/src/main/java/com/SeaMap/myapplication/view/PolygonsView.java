8009dea3709110e3ba06333064bb38757d700e78
Phuong Phung <pkp013@gmail.com>
2020-05-05 09:08:54 +0700

    update

--- app/src/main/java/com/SeaMap/myapplication/view/PolygonsView.java
+++ app/src/main/java/com/SeaMap/myapplication/view/PolygonsView.java
@@ -103,8 +103,8 @@ public class PolygonsView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         scrCtY = h / 2;
         scrCtX = w / 2;
-        pointTopRight = ConvScrPointToWGS(scrCtX * 2,0);
-        pointBotLeft = ConvScrPointToWGS(0, scrCtY * 2);
+        pointTopRight = ConvScrPointToWGS(scrCtX * 3,-scrCtY);
+        pointBotLeft = ConvScrPointToWGS(-scrCtX, scrCtY * 3);
         initPaints();
         mapOutdated = true;
     }
@@ -166,7 +166,7 @@ public class PolygonsView extends View {
     }
     void DrawTextMap()
     {
-        cusPaint.setAntiAlias(true);
+        //cusPaint.setAntiAlias(true);
         cusPaint.setStyle(Paint.Style.STROKE);
         cusPaint.setColor(Color.rgb(255, 239, 213));
         cusPaint.setStyle(Paint.Style.FILL);
@@ -252,12 +252,14 @@ public class PolygonsView extends View {
     private void drawMap()
     {
         if(!paintParamsReady)return;
+
         mapOutdated = false;
+        pointTopRight = ConvScrPointToWGS(scrCtX * 3,-scrCtY);
+        pointBotLeft = ConvScrPointToWGS(-scrCtX, scrCtY * 3);
         long tStart = System.currentTimeMillis();
-        isBufferBusy=true;
+        if(isBufferBusy){isBufferBusy = false; return;}
+        else isBufferBusy=true;
         canvasBuf.drawColor(Color.WHITE);
-        pointTopRight = ConvScrPointToWGS(scrCtX * 2,0);
-        pointBotLeft = ConvScrPointToWGS(0, scrCtY * 2);
         //DRAW POLYGON
         if(ReadFile.dataReady)
         for(int lon = (int) pointBotLeft.x - 2; lon<= (int) pointTopRight.x + 2; lon++) {
